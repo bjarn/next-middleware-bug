35eee1b683ab680d85b3c83c416a876752d20630
Bjarn Bronsveld <git@bjarn.net>
2022-07-01 15:42:38 +0200

    remove tailwind and update to canary

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { AppProps } from "next/app";
-import "css/tailwind.css";
 import { appWithTranslation } from "next-i18next";
 
 function App({ Component, pageProps }: AppProps) {
